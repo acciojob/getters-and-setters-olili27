46bb4d81ca42e8c25de4fc06cffe97604a9442ae
Timothy Olili <timmolili27@gmail.com>
2024-10-16 14:34:11 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age) {
+		this._name = name;
+		this._age = age;
+	}
 
-class Student extends Person {}
+	get name() {
+		return this._name;
+	}
 
-class Teacher extends Person {}
+	set age(age) {
+		this._age = age
+	}
+}
+
+class Student extends Person {
+	constructor(name, age) {
+		super(name, age);
+	}
+
+	study() {
+		return this.name + " is studying"
+	}
+}
+
+class Teacher extends Person {
+	constructor(name, age) {
+		super(name, age);
+	}
+
+	teach() {
+		return this.name + " is teaching"
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
